cd530896b597d13172869051f530f2819c8a1418
Noah Rodgers <noah@createdbynoah.com>
2023-10-21 16:15:33 -0500

    attempt photo orientation logic

--- src/components/image/FaceRecognition.jsx
+++ src/components/image/FaceRecognition.jsx
@@ -1,16 +1,28 @@
+import { useState, useEffect } from 'react';
 import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
+  const [orientation, setOrientation] = useState(null);
+
+  useEffect(() => {
+    const image = document.getElementById('inputImage');
+    if (image) {
+      image.addEventListener('load', () => {
+        const width = image.naturalWidth;
+        const height = image.naturalHeight;
+        if (width > height) {
+          setOrientation('landscape');
+        } else {
+          setOrientation('portrait');
+        }
+      });
+    }
+  }, [imageUrl]);
+
   return (
     <div className="image-container2">
       <div className="image-container">
-        <img
-          id="inputImage"
-          src={imageUrl}
-          alt=""
-          width="600px"
-          height="auto"
-        />
+        <img id="inputImage" src={imageUrl} alt="" />
         {boxes.map((box) => (
           <div
             key={box.id}
